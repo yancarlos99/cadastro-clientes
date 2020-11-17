e0a5aa71ed2f52d28f6eaf3645c78e798c1b7434
Yan Carlos <yancarlos_99@outlook.com>
2020-11-17 02:16:49 -0300

    Commit de implantação do lombok e da entitdade de serviço

--- backend/src/main/java/br/com/cadastro/clientes/model/entity/Servico.java
+++ backend/src/main/java/br/com/cadastro/clientes/model/entity/Servico.java
@@ -1,29 +1,33 @@
 package br.com.cadastro.clientes.model.entity;
 
-import java.time.LocalDate;
+import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
 @Entity
 @Data
-public class Cliente {
+public class Servico {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(nullable = false, length = 150)
-	private String nome;
+	private String descricao;
 	
-	@Column(nullable = false, length = 11)
-	private String cpf;
+	@ManyToOne
+	@JoinColumn(name = "id_cliente")
+	private Cliente cliente;
 	
-	@Column(name = "data_cadastro")
-	private LocalDate dataCadastro;
+	@Column
+	private BigDecimal valor;
+
 }
