a1d8a07bb9197281fc4cc9a78c32f9a672b6b4e0
Yan Carlos <yancarlos_99@outlook.com>
2020-11-19 15:36:07 -0300

    Implementação dos métodos e finalização do back-end

--- backend/src/main/java/br/com/cadastro/clientes/model/entity/Cliente.java
+++ backend/src/main/java/br/com/cadastro/clientes/model/entity/Cliente.java
@@ -7,23 +7,45 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotEmpty(message = "{campo.nome.obrigatorio}")
 	@Column(nullable = false, length = 150)
 	private String nome;
 	
+	@NotNull(message = "{campo.cpf.obrigatorio}")
+	@CPF(message = "{campo.cpf.invalido}")
 	@Column(nullable = false, length = 11)
 	private String cpf;
 	
-	@Column(name = "data_cadastro")
+	@Column(name = "data_cadastro", updatable = false)
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataCadastro;
+	
+	@PrePersist
+	public void prePrersist() {
+		setDataCadastro(LocalDate.now());
+	}
 }
---
+++ backend/src/main/java/br/com/cadastro/clientes/rest/ClienteController.java
@@ -0,0 +1,61 @@
+package br.com.cadastro.clientes.rest;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.com.cadastro.clientes.model.entity.Cliente;
+import br.com.cadastro.clientes.model.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente salvar(@RequestBody @Valid Cliente cliente) {
+		return repository.save(cliente);
+	}
+	
+	@GetMapping("{id}")
+	public Cliente acharPorId(@PathVariable Long id) {
+		return repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "CLIENTE NÃO ENCONTRADO"));
+	}
+
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long id) {
+		repository
+		.findById(id)
+		.map(cliente -> {
+			repository.delete(cliente);
+			return Void.TYPE;
+		})
+		.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "CLIENTE NÃO ENCONTRADO"));
+	}
+	
+	@PutMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizar(@PathVariable Long id, @RequestBody Cliente clienteAtualizado) {
+		repository.findById(id)
+		.map(cliente -> {
+			cliente.setNome(clienteAtualizado.getNome());
+			cliente.setCpf(clienteAtualizado.getCpf());
+			return repository.save(clienteAtualizado);
+		}).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "CLIENTE NÃO ENCONTRADO"));
+	}
+}
